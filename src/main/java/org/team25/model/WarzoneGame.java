76b8c576cd398827828c721f05da11ecd78b693a
BhartiChh <bharticon210586@gmail.com>
2024-02-14 00:32:48 -0500

    modified the structure

--- src/main/java/org/team25/model/WarzoneGame.java
+++ src/main/java/org/team25/model/WarzoneGame.java
@@ -14,10 +14,7 @@ import java.util.logging.Logger;
  */
 public class WarzoneGame {
 
-    /**
-     * A data member to trigger Game Engine
-     */
-    private GameEngine d_GameEngine;
+
     /**
      * A data member that will log the data for the class
      */
@@ -34,6 +31,11 @@ public class WarzoneGame {
     private static GamePhase d_GamePhase;
 
     /**
+     * A data member to hold GameEngine
+     */
+    private GameEngine d_GameEngine;
+
+    /**
      * main method to start game
      *
      * @param args the arguments
@@ -42,15 +44,15 @@ public class WarzoneGame {
     public static void main(String[] args) throws Exception {
         Scanner l_Scanner = new Scanner(System.in);
         d_Logger.log(d_logLevel,"");
-        d_Logger.log(d_logLevel,"*********************************");
-        d_Logger.log(d_logLevel,"Welcome to Warzone Game");
-        d_Logger.log(d_logLevel,"*********************************");
-        d_Logger.log(d_logLevel,"Choose the option from Main Menu");
-        d_Logger.log(d_logLevel,"---------------------------------");
+        d_Logger.log(d_logLevel,"************************************");
+        d_Logger.log(d_logLevel,"Welcome to Warzone Game,Let's Play !");
+        d_Logger.log(d_logLevel,"************************************");
+        d_Logger.log(d_logLevel,"Choose option from Main-Menu to play");
+        d_Logger.log(d_logLevel,"------------------------------------");
         d_Logger.log(d_logLevel,"1. New Game to play");
         d_Logger.log(d_logLevel,"2. Load Game to continue");
         d_Logger.log(d_logLevel,"5. Exit");
-        d_Logger.log(d_logLevel,"---------------------------------");
+        d_Logger.log(d_logLevel,"------------------------------------");
         try {
             int option = l_Scanner.nextInt();
             switch (option) {
