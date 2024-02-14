399d0d9b53c2ae55e216ecb30bcdd53579a7b4ed
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-02-14 17:39:10 -0500

    #feature/alpha_tejasvi
    - Merged release/alpha into release/alpha_tejasvi

---
+++ src/main/java/org/team25/game/controllers/ShowMap.java
@@ -0,0 +1,20 @@
+package org.team25.game.controllers;
+
+import org.team25.game.models.GMap;
+
+
+public class ShowMap {
+
+    private GMap d_gMap;
+
+    /**
+     * Constructor to set the current GMap object to read and show the map
+     * @param p_gMap GMap Object Parameter
+     */
+    ShowMap(GMap p_gMap){
+        this.d_gMap = p_gMap;
+    }
+
+
+
+}
