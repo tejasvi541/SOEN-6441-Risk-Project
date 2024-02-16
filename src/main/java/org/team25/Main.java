1e057586775f8064ccb5bd19234a27ad64b142d8
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-02-16 09:00:40 -0500

    #feature/alpha_tejasvi
    - Merged code from #feature/alpha_kapil and omitted merge conflicts

--- src/main/java/org/team25/Main.java
+++ src/main/java/org/team25/Main.java
@@ -1,22 +1,26 @@
 package org.team25;
 
+import org.team25.game.controllers.MapEditorController;
+import org.team25.game.controllers.MapLoaderController;
+import org.team25.game.controllers.ShowMapController;
 import org.team25.game.game_engine.GameEngine;
+import org.team25.game.models.map.GameMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        new GameEngine().start();
-//        GameMap gameMap = new GameMap();
-//        MapLoaderController load = new MapLoaderController();
-//
-//        gameMap = load.readMap("Canada");
-//
-//        ShowMapController showMapController = new ShowMapController(gameMap);
-//        showMapController.show(gameMap);
-//
-//        MapEditorController editor = new MapEditorController(gameMap);
-//
-//        editor.run();
+//        new GameEngine().start();
+        GameMap gameMap = new GameMap();
+        MapLoaderController load = new MapLoaderController();
+
+        gameMap = load.readMap("Canada");
+
+        ShowMapController showMapController = new ShowMapController(gameMap);
+        showMapController.show(gameMap);
+
+        MapEditorController editor = new MapEditorController(gameMap);
+
+        editor.run();
 
     }
 }
