6645cd886e898b75f7bded75fc3b5fadd80256ed
BhartiChh <bharticon210586@gmail.com>
2024-02-15 22:12:15 -0500

    #feature/alpha_bharati
    - added description to interface methods

--- src/main/java/org/team25/game/interfaces/MapEditor.java
+++ src/main/java/org/team25/game/interfaces/MapEditor.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 public interface MapEditor {
 
+    /**
+     * To take action on input map command
+     * @param p_List to hold user input command
+     * @return boolean: true if edit is successful
+     */
     public abstract boolean action(List<String> p_List);
+
+    /**
+     * To validate the user input command
+     * @param p_InputList to hold user input command
+     * @return boolean: true if command is valid CLI command
+     */
     public abstract boolean validateUserInput(List<String> p_InputList);
 }
