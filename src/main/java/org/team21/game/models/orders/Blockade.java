f339d213b6b1b81607f2cc7c355e55ce7a44aa9c
nishith-soni <nishithsoni4444@gmail.com>
2024-03-13 15:32:13 -0400

    Made function validateCommand.

--- src/main/java/org/team21/game/models/orders/Blockade.java
+++ src/main/java/org/team21/game/models/orders/Blockade.java
@@ -17,7 +17,7 @@ public class Blockade extends Order{
     @Override
     public boolean execute() {
         Player l_Player = getOrderInfo().getPlayer();
-        Country l_Country = getOrderInfo().getDestination();
+        Country l_Country = getOrderInfo().getTargetCountry();
         if(validateCommand()){
             l_Country.setArmies(l_Country.getArmies() * 3);
             l_Player.getCapturedCountries().remove(l_Country);
@@ -31,7 +31,21 @@ public class Blockade extends Order{
 
     @Override
     public boolean validateCommand() {
-        return false;
+        Player l_Player = getOrderInfo().getPlayer();
+        Country l_Country = getOrderInfo().getTargetCountry();
+        if(l_Player == null){
+            System.err.println("The Player is not valid.");
+            return false;
+        }
+        if(l_Country.getPlayer() != l_Player){
+            System.err.println("The target country does not belong to the player.");
+            return false;
+        }
+        if(!l_Player.checkIfCardAvailable(CardType.BLOCKADE)){
+            System.err.println("Player doesn't have Blockade Card.");
+            return false;
+        }
+        return true;
     }
 
     @Override
