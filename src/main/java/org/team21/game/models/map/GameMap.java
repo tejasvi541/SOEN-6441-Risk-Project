60a301d6e82f033367b20b1ba07858c8c64938a7
Kapil Soni <kapssoni333@gmail.com>
2024-04-02 17:40:55 -0400

    #feature/kapil_beta - Changed file locations

--- src/main/java/org/team21/game/models/map/GameMap.java
+++ src/main/java/org/team21/game/models/map/GameMap.java
@@ -2,7 +2,8 @@ package org.team21.game.models.map;
 
 import org.team21.game.game_engine.GamePhase;
 import org.team21.game.models.strategy.player.PlayerStrategy;
-import org.team21.game.utils.*;
+import org.team21.game.utils.adapter.Adaptee;
+import org.team21.game.utils.adapter.Adapter;
 import org.team21.game.utils.logger.LogEntryBuffer;
 import org.team21.game.utils.validation.MapValidation;
 import org.team21.game.utils.validation.ValidationException;
