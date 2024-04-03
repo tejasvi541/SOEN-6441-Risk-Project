06de255bc998eb0817ac1aaf95c8a45c812e9fe0
shahyesha <yeshashah951@gmail.com>
2024-04-03 15:47:35 -0400

    authors for observer

--- src/main/java/org/team21/game/interfaces/observers/Observer.java
+++ src/main/java/org/team21/game/interfaces/observers/Observer.java
@@ -2,7 +2,7 @@ package org.team21.game.interfaces.observers;
 
 /**
  * An interface for implementation of Observer with an update function
- *
+ * @author Meet Boghani
  */
 public interface Observer {
 
